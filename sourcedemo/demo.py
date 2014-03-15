8f0c19b91342a40418318c667f4a5572d54064e4
Nick Timkovich <prometheus235@gmail.com>
2014-03-15 03:01:06 -0500

    Parser processes entire demo without errors, but not much data is being saved.

--- sourcedemo/demo.py
+++ sourcedemo/demo.py
@@ -12,7 +12,7 @@ MAX_OSPATH = 260
 HEADER_MAGIC = b'HL2DEMO\x00'
 HEADER_FIELDS = [
     # Field                 Type      Value
-    ('magic',               '8s'),  # should be "HL2DEMO"+NULL
+    #('magic',               '8s'),  # should be "HL2DEMO"+NULL
     ('demo_protocol',       'i'),   # Demo protocol version
     ('network_protocol',    'i'),   # Network protocol version number
     ('server_name',         '{}s'.format(MAX_OSPATH)),
@@ -25,24 +25,35 @@ HEADER_FIELDS = [
     ('sign_on_length',      'i'),   # Length of the signon data (Init for first frame)
 ]
 
-PACKET_TYPES = {
-    b'\x01': 'sign_on',
-    b'\x02': 'packet',
-    b'\x03': 'sync_tick',
-    b'\x04': 'console_cmd',
-    b'\x05': 'user_cmd',
-    b'\x06': 'datatables',
-    b'\x07': 'stop',
-    b'\x08': 'custom_data',
-    b'\x09': 'string_tables',
-}
+CMD_SIGN_ON = 1
+CMD_PACKET = 2
+CMD_SYNC_TICK = 3
+CMD_CONSOLE_CMD = 4
+CMD_USER_CMD = 5
+CMD_DATATABLES = 6
+CMD_STOP = 7
+CMD_CUSTOM_DATA = 8
+CMD_STRING_TABLES = 9
 
-header_field_names, header_field_types = zip(*HEADER_FIELDS)
-header_struct = ''.join(header_field_types)
-
-def struct_read(struct_fmt, handle):
-    data = handle.read(struct.calcsize(struct_fmt))
-    return struct.unpack(struct_fmt, data)
+class BinReader(io.FileIO):
+    def read_binary(self, fmt):
+        data = self.read(struct.calcsize(fmt))
+        return struct.unpack(fmt, data)
+    def read_chr(self):
+        return self.read_binary('c')[0]
+    def read_byte(self):
+        return self.read_binary('B')[0]
+    def read_int(self):
+        return self.read_binary('i')[0]
+    def read_float(self):
+        return self.read_binary('f')[0]
+    def read_string(self, length, trim_null=False):
+        sb = self.read_binary(str(length) + 's')[0]
+        if trim_null:
+            sb = sb.rstrip(b'\x00')
+        return sb
+    def skip(self, delta):
+        self.seek(delta, io.SEEK_CUR)
 
 class Demo():
     """
@@ -50,41 +61,87 @@ class Demo():
     https://developer.valvesoftware.com/wiki/DEM_Format
     """
     def __init__(self, filepath):
-        num_packets = 0
-        with open(filepath, 'rb') as f:
-            header_data = f.read(struct.calcsize(header_struct))
-            self.header = dict(zip(header_field_names, struct.unpack(header_struct, header_data)))
-            if self.header['magic'] != HEADER_MAGIC:
-                raise Exception("The specified file doesn't seem to be a demo.")
-            del self.header['magic']
+        self.demo = BinReader(filepath)
+        magic = self.demo.read_string(8)
+        if magic != HEADER_MAGIC:
+            raise Exception("The specified file doesn't seem to be a demo.")
 
-            
-            # while True:
-            #     if num_packets:
-            #         packet_len = struct.unpack('i', f.read(4))[0]
-            #     else:
-            #         packet_len = self.header['sign_on_length']
-            #     packet_data = f.read(packet_len - 4)# - struct.calcsize(header_struct) + 4)
-
-            #     num_packets += 1
-            #     print(num_packets, packet_len, packet_data[:min(len(packet_data), 10)])
+        self.header = {name: self.demo.read_binary(fmt)[0] for name, fmt in HEADER_FIELDS}
 
         # stringify and trim bytestrings
         for field in self.header:
             if isinstance(self.header[field], bytes):
                 self.header[field] = self.header[field].decode().rstrip('\x00')
 
-        # pprint(self.header)
+        while True:
+            command = self.demo.read_byte()
+            if command == CMD_STOP:
+                break
+            # if command is a stop, break
 
-        # with open(filepath, 'rb') as f:
-        #     f.seek(0x3354A - 8)
-        #     server_frame, client_frame, subpacket_size = readout(f, 'iii')
+            tick = self.demo.read_int()
 
-        #     subpacket_data = f.read(subpacket_size)
+            self.demo.skip(1) # unknown
 
-        #     while True:
-        #         command = f.read(1)
-        #         command_num = readout(f, 'i')
+            self._process_command(command)
+
+    def _process_command(self, command):
+        {
+            CMD_SIGN_ON: self._process_sign_on,
+            CMD_PACKET: self._process_packet,
+            CMD_SYNC_TICK: self._process_sync_tick,
+            CMD_CONSOLE_CMD: self._process_console_cmd,
+            CMD_USER_CMD: self._process_user_cmd,
+            CMD_DATATABLES: self._process_data_tables,
+            CMD_CUSTOM_DATA: self._process_custom_data,
+            CMD_STRING_TABLES: self._process_string_tables,
+        }[command]()
+
+    def _process_sign_on(self):
+        """Sign on packet: Ignore"""
+        print('Sign On')
+        self.demo.skip(self.header['sign_on_length'])
+
+    def _process_packet(self):
+        """Network packet: Get position data"""
+        self.demo.skip(4) # unknown
+        x, y, z = self.demo.read_binary('fff')
+        self.demo.skip(0x90) # unknown
+
+        cmd_len = self.demo.read_int()
+        self.demo.skip(cmd_len) # ignore it all
+
+        return x, y, z
+
+    def _process_sync_tick(self):
+        """Never happens?  Means nothing?"""
+        pass
+
+    def _process_console_cmd(self):
+        """Console command: Returns the command"""
+        cmd_len = self.demo.read_int()
+        console_cmd = self.demo.read_string(cmd_len)
+
+        return console_cmd
+
+    def _process_user_cmd(self):
+        """User command: Ignore"""
+        self.demo.skip(4)
+        self.demo.skip(self.demo.read_int())
+
+    def _process_data_tables(self):
+        """Data tables command: Unimplemented"""
+        raise NotImplementedError()
+
+    def _process_string_tables(self):
+        """String tables: Ignore"""
+        self.demo.skip(self.demo.read_int())
+        
+
+    def _process_custom_data(self):
+        """Custom data: Ignore"""
+        self.demo.skip(4)
+        self.demo.skip(self.demo.read_int())
 
 if __name__ == '__main__':
     import argparse
